550069b6fa6a35c2d841cddf3f004e61d5e75317
ktp <kato@giftee.co>
2015-10-24 15:48:24 +0900

    [modify] enable to display image repeatedly.

--- Picchannel/ViewController.swift
+++ Picchannel/ViewController.swift
@@ -17,28 +17,43 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         do {
-            // urlから画像を取得
-            let url = NSURL(string: "https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s640x640/sh0.08/e35/12139835_481785788659634_1326639266_n.jpg");
+            // 画像urlを設定する.
+            var urls : [String] = [
+                "https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s320x320/e35/12070661_1728238167404162_154574692_n.jpg",
+                "https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s320x320/e35/11337185_773036789492157_1799302479_n.jpg",
+                "https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s320x320/e35/12093269_1664014217215940_2028057166_n.jpg"
+            ]
             
-            let imageData :NSData = try NSData(contentsOfURL: url! ,options: NSDataReadingOptions.DataReadingMappedIfSafe)
+            var y: CGFloat = 0
+            
+            for i in 0...(urls.count - 1) {
+                
+                // urlから画像を取得する.
+                let url = NSURL(string: urls[i]);
+                let imageData :NSData = try NSData(contentsOfURL: url! ,options: NSDataReadingOptions.DataReadingMappedIfSafe)
 
-            // UIImageに画像を設定する.
-            let myImage = UIImage(data: imageData)!
-            
-            // UIImageViewを生成する.
-            let myImageView = UIImageView()
-            
-            // myImageViewのimageにmyImageを設定する.
-            myImageView.image = myImage
-            
-            // frameの値を設定する.
-            myImageView.frame = CGRectMake(0, 0, myImage.size.width, myImage.size.height)
-            
-            // ScrollViewにmyImageViewを追加する.
-            scrollView.addSubview(myImageView)
-            
-            // ScrollViewにcontentSizeを設定する.
-            scrollView.contentSize = CGSizeMake(myImageView.frame.size.width, myImageView.frame.size.height)
+                // UIImageに画像を設定する.
+                let myImage = UIImage(data: imageData)!
+                
+                // UIImageViewを生成する.
+                let myImageView = UIImageView()
+                
+                // myImageViewのimageにmyImageを設定する.
+                myImageView.image = myImage
+                
+                // frameの値を設定する.
+                myImageView.frame = CGRectMake(0, y, myImage.size.width, myImage.size.height)
+                
+                // ScrollViewにmyImageViewを追加する.
+                scrollView.addSubview(myImageView)
+                
+                // 画像サイズを取得する.
+                y += myImage.size.height + 0.5
+                
+                // ScrollViewにcontentSizeを設定する.
+                scrollView.contentSize = CGSizeMake(myImageView.frame.size.width, y)
+                
+            }
             
         } catch {
             // error出力
