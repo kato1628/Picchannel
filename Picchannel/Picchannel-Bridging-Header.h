ba386de9779f9744ef562d2da83813caea2d81c3
ktp <kato@giftee.co>
2015-11-22 21:59:50 +0900

    [fix] modify bridging header file.

--- Picchannel/Picchannel-Bridging-Header.h
+++ Picchannel/Picchannel-Bridging-Header.h
@@ -1,7 +1,7 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
-#import <MRProgress/MRProgress.h>
-#import <InstagramKit/InstagramKit.h>
-#import <SDWebImage/UIImageView+WebCache.h>
-#import <MNMBottomPullToRefresh/MNMBottomPullToRefreshManager.h>
+#import <MRProgress.h>
+#import <InstagramKit.h>
+#import <UIImageView+WebCache.h>
+#import <MNMBottomPullToRefreshManager.h>
